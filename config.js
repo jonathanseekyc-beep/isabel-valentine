5472b9b8eb9476f2099f8b4b4b6569dddbcbd536
jonathanseekyc-beep <jonathanseekyc@gmail.com>
2026-02-13 01:40:07 +0800

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
         musicUrl: "https://res.cloudinary.com/dtxv5zne6/video/upload/v1770917810/wave_to_earth_-_sunny_days_pqwpit.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 1.0                        // Volume level (0.0 to 1.0)
     }
 };
 
